92d6833bca3c93a6711f4eb7c18d31d9ece8bcff
王强 <wangqiang@medlive.cn>
2018-05-14 10:20:48 +0800

    update

--- cookie/page1.php
+++ cookie/page1.php
@@ -1,54 +1,53 @@
 <?php
-$value = 'something from somewhere';
-setcookie("TestCookie", $value, time()-1, '/');
-echo $_COOKIE["TestCookie"];die;
-// 打印一个单独的 Cookie
-// echo $_COOKIE["TestCookie"] . PHP_EOL;
-//  debug/test 查看所有 Cookie 的另一种方式
-// print_r($_COOKIE);
-// session_start();
-// echo session_id();
-// session_commit();
+// $value = 'something from somewhere';
+// setcookie("TestCookie", $value, time()-1, '/');
+// echo $_COOKIE["TestCookie"];die;
+// // 打印一个单独的 Cookie
+// // echo $_COOKIE["TestCookie"] . PHP_EOL;
+// //  debug/test 查看所有 Cookie 的另一种方式
+// // print_r($_COOKIE);
+// // session_start();
+// // echo session_id();
+// // session_commit();
 // 
 ?>
 <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-
-<style>
-	.wx_shadow { background-color: #ddd;  position: fixed; width: 100%; height: 100%; z-index: 100; opacity: 0.5;left: 0;top: 0;}
-	.wx_pop_box { background-color: #fff;  position: fixed; width: 300px; height: 200px; z-index: 100; top:39%; left:39%; border-radius: 5px;}
-	.wx_pop_cont {text-align: center; height: 150px; width: 300px; line-height: 150px; font-size: 20px;}
-	.wx_pop_btm {width:300px; line-height: 50px; text-align: center; }
-	/*input {border: 1px #ddd solid; line-height: 40px; background-color: #fff; width: 100%;margin-bottom: 200px;}*/
-	input {
-		/*background-color: #4CAF50;*/
-		background-color: #fff;
-	    /*border: none;*/
-	    border: 1px solid #ddd;
-	    /*color: white;*/
-	    padding: 15px 32px;
-	    margin: 0px 20px;
-	    text-align: center;
-	    text-decoration: none;
-	    display: inline-block;
-	    font-size: 16px;
-	    line-height: 6px;
-	    border-radius: 5px;
-	    cursor:pointer;
-	}
-</style>
-<div class="wx_shadow"></div>
-<div class="wx_pop_box">
-	<div class="wx_pop_cont">
-		是否要绑定微信帐号？
-	</div>
-	<div class="wx_pop_btm">
-		<input class="" type="button" value="确定">
-		<input class="" type="button" value="取消">
-	</div>
-</div>
-
 <script>
-	$(function() {
+// setWxCookie("_wx_window_click_");
+delWxCookie("_wx_window_click_");
+if (getWxCookie('_wx_window_click_') == null) { // || getWxCookie('_wx_window_click_') == null // null不能用===
+	alert("无");
+} else {
+	alert("有");
+}
 
-	});
+// 存cookie，记录弹框
+function setWxCookie(name)
+{
+    var date = new Date();  //获取当前时间
+    date.setTime(date.getTime()+300*1000); //获取30秒后的时间戳
+    document.cookie = name+"=1;expires="+date.toGMTString();
+}
+
+// 定义一个函数，用来读取特定的cookie值。
+function getWxCookie(cookie_name)
+{
+    var cookies = document.cookie ? document.cookie.split(';') : [];
+    var c;
+    for(var i=0; i<cookies.length; i++){
+        c = cookies[i].split('=');
+        if (c[0].replace(' ', '') === cookie_name) {
+            return c[1];
+        }
+    }
+}
+
+function delWxCookie(name)
+{
+	var exp = new Date();
+	exp.setTime(exp.getTime() - 1);
+	var cval = getWxCookie(name);
+	if(cval != null)
+	document.cookie= name + "="+cval+";expires="+exp.toGMTString();
+}
 </script>
