7f6d5029a7e7b45502a205ac4f92d2db6a10ab1b
王强 <wangqiang@medlive.cn>
2018-06-06 09:47:25 +0800

    css

---
+++ html/flex/demo2/demo1.php
@@ -0,0 +1,12 @@
+<style>
+.flex{display:flex;width:600px;margin:0;padding:0;list-style:none;}
+.flex li:nth-child(1){width:200px;background-color:red;}
+.flex li:nth-child(2){flex-grow:1;width:50px;background-color:green;}
+.flex li:nth-child(3){flex-grow:3;width:50px;background-color:#cccccc;}
+</style>
+<ul class="flex">
+	<li>a</li>
+	<li>b</li>
+	<li>c</li>
+</ul>
+
