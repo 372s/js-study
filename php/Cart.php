e6432bd7e442f90e6150fa7ef5064edfae55f8b7
王强 <wangqiang@medlive.cn>
2018-06-20 10:17:36 +0800

    cart

--- php/Cart.php
+++ php/Cart.php
@@ -1,34 +1,33 @@
 <?php
 class Cart
 {
-    const PRICE_BUTTER  = 1.00;
-    const PRICE_MILK    = 3.00;
-    const PRICE_EGGS    = 6.95;
+    const PRICE_BUTTER = 1.00;
+    const PRICE_MILK = 3.00;
+    const PRICE_EGGS = 6.95;
 
     public $products = array();
-    
+
     public function add($product, $quantity)
     {
         $this->products[$product] = $quantity;
     }
-    
+
     public function getQuantity($product)
     {
         return isset($this->products[$product]) ? $this->products[$product] :
-               FALSE;
+        false;
     }
-    
+
     public function getTotal($tax)
     {
         $total = 0.00;
-        
+
         $callback =
-            function ($quantity, $product) use ($tax, &$total)
-            {
-                $pricePerItem = constant(__CLASS__ . "::PRICE_" . strtoupper($product));
-                $total += ($pricePerItem * $quantity) * ($tax + 1.0);
-            };
-        
+        function ($quantity, $product) use ($tax, &$total) {
+            $pricePerItem = constant(__CLASS__ . "::PRICE_" . strtoupper($product));
+            $total += ($pricePerItem * $quantity) * ($tax + 1.0);
+        };
+
         array_walk($this->products, $callback);
         return round($total, 2);
     }
@@ -41,5 +40,5 @@ $my_cart->add('milk', 3);
 $my_cart->add('eggs', 6);
 
 // 打出出总价格，其中有 5% 的销售税.
-// print $my_cart->getTotal(0.05) . "\n";
+print $my_cart->getTotal(0.05) . "\n";
 // 最后结果是 54.29
